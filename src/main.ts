4fc9b3feb4ccf2ef40ee14689e1eb6fcd9d5e93f
Anuar7777 <aman_anuar@mail.ru>
2025-07-28 01:44:18 +0500

    refactor: replace process.env with ConfigService in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
 import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 const logger = new Logger('Bootstrap')
 
@@ -9,15 +10,17 @@ async function bootstrap() {
 	try {
 		const app = await NestFactory.create(AppModule)
 
+		const configService = app.get(ConfigService)
+
 		app.setGlobalPrefix('api')
 		app.use(cookieParser())
 		app.enableCors({
-			origin: [process.env.CLIENT_URL ?? 'http://localhost:3000'],
+			origin: [configService.get('CLIENT_URL', 'http://localhost:3000')],
 			credentials: true,
 			exposedHeaders: 'set-cookie',
 		})
 
-		await app.listen(process.env.PORT ?? 5000)
+		await app.listen(configService.get('PORT', 5000))
 	} catch (error) {
 		logger.error('Error when starting the server: ', error)
 		process.exit(1)
